519e0f21426dab071bcad4eb93b3f4bc2b8fcde3
IDMiller2020 <psiauto@gmail.com>
2021-03-24 16:31:04 -0600

    Added color changes to winner card

--- app.js
+++ app.js
@@ -17,19 +17,35 @@ function  computerChoice() {
 function playGame(choice) {
   compRPS = computerChoice()
   document.getElementById('winnerCard').classList.remove('d-none')
+  document.getElementById('wcb').classList.remove('bg-warning','bg-success', 'bg-danger')
+
   if (choice === compRPS) {
+    document.getElementById('wcb').classList.remove('bg-*')
     document.getElementById('winner').innerText = 'Tie!'
+    document.getElementById('wcb').classList.add('bg-warning')
   } else if (choice === 'ROCK' && compRPS === 'PAPER') {
+    document.getElementById('wcb').classList.remove('bg-*')
     document.getElementById('winner').innerText = 'The Computer is the winner!'
+    document.getElementById('wcb').classList.add('bg-danger')
   } else if (choice === 'ROCK' && compRPS === 'SCISSORS') {
+    document.getElementById('wcb').classList.remove('bg-*')
     document.getElementById('winner').innerText = 'You are the winner!'
+    document.getElementById('wcb').classList.add('bg-success')
   } else if (choice === 'PAPER' && compRPS === 'ROCK') {
+    document.getElementById('wcb').classList.remove('bg-*')
     document.getElementById('winner').innerText = 'You are the winner!'
+    document.getElementById('wcb').classList.add('bg-success')
   } else if (choice === 'PAPER' && compRPS === 'SCISSORS') {
+    document.getElementById('wcb').classList.remove('bg-*')
     document.getElementById('winner').innerText = 'The Computer is the winner!'
+    document.getElementById('wcb').classList.add('bg-danger')
   } else if (choice === 'SCISSORS' && compRPS === 'ROCK') {
+    document.getElementById('wcb').classList.remove('bg-*')
   document.getElementById('winner').innerText = 'The Computer winner!'
+  document.getElementById('wcb').classList.add('bg-danger')
   } else if (choice === 'SCISSORS' && compRPS === 'PAPER') {
+    document.getElementById('wcb').classList.remove('bg-*')
     document.getElementById('winner').innerText = 'You are the winner!'
+    document.getElementById('wcb').classList.add('bg-success')
   }
 }
