5397936a49342a4850e0008fa000254863224df1
IDMiller2020 <psiauto@gmail.com>
2021-03-24 16:12:39 -0600

    CSS and Bootstrap modifications

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 let compRPS = ''
-let userRPS = ''
 
 function  computerChoice() {
   let randomChoice = 0
   randomChoice = (Math.floor(Math.random() * 3) + 1)
-  console.log(`random number: ${randomChoice}`)
-
   if (randomChoice === 1) {
     compRPS = 'ROCK'
   } else if (randomChoice === 2) {
@@ -19,22 +16,20 @@ function  computerChoice() {
 
 function playGame(choice) {
   compRPS = computerChoice()
-  console.log(compRPS)
-  userRPS = choice
-  console.log(`You chose ${userRPS} and the computer chose ${compRPS}.`)
-  if (userRPS === compRPS) {
+  document.getElementById('winnerCard').classList.remove('d-none')
+  if (choice === compRPS) {
     document.getElementById('winner').innerText = 'Tie!'
-  } else if (userRPS === 'ROCK' && compRPS === 'PAPER') {
+  } else if (choice === 'ROCK' && compRPS === 'PAPER') {
     document.getElementById('winner').innerText = 'The Computer is the winner!'
-  } else if (userRPS === 'ROCK' && compRPS === 'SCISSORS') {
+  } else if (choice === 'ROCK' && compRPS === 'SCISSORS') {
     document.getElementById('winner').innerText = 'You are the winner!'
-  } else if (userRPS === 'PAPER' && compRPS === 'ROCK') {
+  } else if (choice === 'PAPER' && compRPS === 'ROCK') {
     document.getElementById('winner').innerText = 'You are the winner!'
-  } else if (userRPS === 'PAPER' && compRPS === 'SCISSORS') {
+  } else if (choice === 'PAPER' && compRPS === 'SCISSORS') {
     document.getElementById('winner').innerText = 'The Computer is the winner!'
-  } else if (userRPS === 'SCISSORS' && compRPS === 'ROCK') {
+  } else if (choice === 'SCISSORS' && compRPS === 'ROCK') {
   document.getElementById('winner').innerText = 'The Computer winner!'
-  } else if (userRPS === 'SCISSORS' && compRPS === 'PAPER') {
+  } else if (choice === 'SCISSORS' && compRPS === 'PAPER') {
     document.getElementById('winner').innerText = 'You are the winner!'
   }
 }
